36d0511570db74f1c3ee3108aacf143321e49ac0
朱力 <790403624@qq.com>
2025-07-30 16:28:32 +0800

    change log to multithreaded to detect more logs

--- Editor/CodeEditorIntegrationCore.cs
+++ Editor/CodeEditorIntegrationCore.cs
@@ -453,7 +453,7 @@ namespace Hackerzhuli.Code.Editor
         }
 
         private void OnLogMessageReceivedThreaded(string logString, string stackTrace, LogType type){
-            //FileLogger.Log($"Log message received threaded: [{type}] {logString}");
+            FileLogger.Log($"Log message received threaded: [{type}] {logString}");
         }
 
         /// <summary>
@@ -485,7 +485,7 @@ namespace Hackerzhuli.Code.Editor
 
             BroadcastMessage(messageType, message);
 
-            FileLogger.Log($"Log message received: [{type}] {message}");
+            //FileLogger.Log($"Log message received: [{type}] {message}");
         }
      
         private void EnsureMessengerInitialized()
